553afdb222693a3b42c6db2a329189548f531131
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-06-14 10:29:59 -0500

    updates server.js and write.html
    creates POST module.
    DB is next.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({extended: true})) 
+
 app.listen(8080, function(){
     console.log('listening on 8080');
-
 });
 
 app.get("/",function(req,res){
@@ -13,6 +14,16 @@ app.get("/",function(req,res){
 app.get("/write",function(req,res){
     res.sendFile(__dirname +"/write.html");
 });
+
+//어떤 사람이 /add 경로로 POST 요청을 하면...
+//???을 해주세요.
+ 
+app.post("/add", function(req,res){
+    res.send("I got the data.");
+    console.log(req.body.title);
+    console.log(req.body.date);
+});
+
 
 //누군가가 /pet 으로 방문을 하면 (get 요청을 하면)
 //pet 관련된 안내문을 띄워주자.
