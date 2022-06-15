ef1a3c3c510b15098a3c5f0ac1f550482ab2b3f6
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-06-15 18:50:32 -0500

    updates databse features
    added MongoDB database feature. Uses now can update and record on write.html

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ToDo App 2022
 
-A simple web app that users can read, write, modify, delete their schedule.
+A web app that users can read, write, modify, delete their schedule.
 
-A simple full stack project using NodeJS, MongoDB, HTML, CSS(Bootstrap).
+A full stack project using NodeJS, MongoDB, HTML, CSS(Bootstrap).
 
 Created by Jiuk Choi
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
 
-app.use(express.urlencoded({extended: true})) 
-
-app.listen(8080, function(){
-    console.log('listening on 8080');
-});
+var db;
+MongoClient.connect('mongodb+srv://admin:qwerty1234@cluster0.twazp.mongodb.net/todoapp?retryWrites=true&w=majority', function(err, client){
+    if (err) return console.log(err)
+    db = client.db('todoapp');
+    // db.collection('post').insertOne({이름:'존',나이:20,_id:101}, (err,result)=>{
+    //     console.log('save completed');
+    // });
+    app.listen(8080, function() {
+      console.log('listening on 8080')
+    });
+  });
+  
+app.use(express.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
@@ -15,14 +25,27 @@ app.get("/write",function(req,res){
     res.sendFile(__dirname +"/write.html");
 });
 
+
+//리스트로 GET요청으로 접속하면
+//실제 DB에 저장된 데이터들로 예쁘게 꾸며진 HTML을 보여줌.
+app.get("/list",(req,res)=>{
+    res.send
+
+});
+
 //어떤 사람이 /add 경로로 POST 요청을 하면...
-//???을 해주세요.
+//폼에 작성한 데이터를 확보한뒤 MongoDB에다가 저장한다.
  
 app.post("/add", function(req,res){
     res.send("I got the data.");
     console.log(req.body.title);
     console.log(req.body.date);
+    db.collection('post').insertOne({WhatToDo:req.body.title,Date:req.body.date}, (err,result)=>{
+        console.log("save completed");
+    })
 });
+
+
 
 
 //누군가가 /pet 으로 방문을 하면 (get 요청을 하면)
